d670c8b35b4dc043df5449995d799a65a23036d5
Brendan Collins <brendan.c.collins@gmail.com>
2015-07-09 16:57:09 -0400

    creating virtual locker

---
+++ sharelockers/src/js/lockerGenerator.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var $ = require('jquery');
+var _ = require('underscore');
+var views = require('views');
+
+
+
+module.exports = function (arr, lockernumbers) {
+  var i = 0;
+
+	while ( i < lockernumbers) {
+
+		var lockerTitle= arr[i].title;
+		var lockerDetails= arr[i].details;
+
+		var squareHtml = '<div class="vlocker"><span class="card animated"><span class="lockerTitle">'+ lockerTitle +'</span><div class="vpopout"><span class="lockerDetails">'+ lockerDetails+'</span></div></div>';
+		console.log(squareHtml);
+		$('.locker-bank').append(squareHtml);		
+
+		
+
+		i++;	 
+
+	};
+
+};

3e746e262b1c382c358ddb41153de6865d14d0cd
Brendan Collins <brendan.c.collins@gmail.com>
2015-07-10 15:24:52 -0400

    populating through api

--- sharelockers/static/js/app.js
+++ sharelockers/static/js/app.js
@@ -2,7 +2,7 @@
 var $ = require("jquery");
 
 $(window).scroll(function() {
-  if ($(this).scrollTop() > 1){  
+  if ($(this).scrollTop() > 1){
     $('.my-page-header').addClass("sticky");
   } else {
     $('.my-page-header').removeClass("sticky");
@@ -12,30 +12,8 @@ $(window).scroll(function() {
 },{"jquery":"jquery"}],2:[function(require,module,exports){
 
 },{}],3:[function(require,module,exports){
-<<<<<<< HEAD
-<<<<<<< HEAD:sharelockers/static/js/app.js
-"use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("",function(){show("home"),$.ajax({method:"GET",url:"/api/profiles/"}).done(function(e){console.log(e)})});
-
-},{"../router":6,"../show":7,"jquery":"jquery","underscore":"underscore","views":"views"}],4:[function(require,module,exports){
-"use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("location/locker",function(){function e(e){var o=views["locker-list"],t=_.template(o,{variable:"m"}),r=t({lockers:e});return $(".main-content").html(r),e}function o(e){var o=null;if(document.cookie&&""!=document.cookie)for(var t=document.cookie.split(";"),r=0;r<t.length;r++){var n=$.trim(t[r]);if(n.substring(0,e.length+1)==e+"="){o=decodeURIComponent(n.substring(e.length+1));break}}return console.log(o),o}$.ajax({method:"GET",url:"/api/lockers/"}).done(function(o){console.log(o),e(o)}),$.ajax({method:"GET",url:"/api/profiles/"}).done(function(e){console.log(e)}),$(document).on("click",".locker-container",function(){var e=this.getAttribute("data-id"),t=this.getAttribute("data-column"),r=this.getAttribute("data-row"),n=o("csrftoken");console.log(n),$.ajax({beforeSend:function(e){console.log(n),e.setRequestHeader("X-CSRFToken",n)},method:"PUT",url:"/api/lockers/"+e,data:{hub:1,row:r,column:t}}).done(function(e){console.log(e)})})});
-
-},{"../router":6,"../show":7,"jquery":"jquery","underscore":"underscore","views":"views"}],5:[function(require,module,exports){
-"use strict";var router=require("./router");require("./animations"),{controllers:{dashboard:require("./controllers/dashboard.js"),home:require("./controllers/home.js"),"locker-list":require("./controllers/locker-list.js")}},router.init();
-
-},{"./animations":1,"./controllers/dashboard.js":2,"./controllers/home.js":3,"./controllers/locker-list.js":4,"./router":6}],6:[function(require,module,exports){
-"use strict";var SortedRouter=require("./sorted-router");module.exports=new SortedRouter;
-
-},{"./sorted-router":8}],7:[function(require,module,exports){
-"use strict";var $=require("jquery"),_=require("underscore"),views=require("views");module.exports=function(e,r){var i=views[e],t=_.template(i,{variable:"m"}),u=t(r);$(".main-content").html(u)};
-
-},{"jquery":"jquery","underscore":"underscore","views":"views"}],8:[function(require,module,exports){
-"use strict";var Backbone=require("backbone"),_=require("underscore"),SortedRouter=Backbone.Router.extend({sortedRoutes:{},route:function(){for(var e=arguments.length-1,t=arguments[arguments.length-1],r=0;e>r;++r)this.sortedRoutes[arguments[r]]=t},init:function(){var e=-1e6,t=this;_.chain(_.pairs(this.sortedRoutes)).sortBy(function(t){var r=t[0];return r.indexOf("*")>=0?e:-r.split(":").length}).each(function(e){Backbone.Router.prototype.route.apply(t,e)}),Backbone.history.start()}});module.exports=SortedRouter;
-
-=======
-=======
 arguments[4][2][0].apply(exports,arguments)
 },{"dup":2}],4:[function(require,module,exports){
->>>>>>> 4a3417439ce5954a4b3c089a7ea931b6c93b311b
 'use strict';
 
 var $ = require('jquery');
@@ -68,7 +46,7 @@ var show = require('../show');
 var lockerGenerator = require('../lockerGenerator');
 
 router.route('location/locker', function () {
-	var arr = [{title : 'teddy', details : 'A Really Big Teddy Bear'}, {title : 'car', details : 'A Super Fast car'}, {title : 'coat', details : 'A leather coat'}, {title : 'flowers', details : '1,000 Roses'}, {title : 'shoes', details : 'Air Jordans, size 9'}, {title : 'marbles', details : 'a million marbles'}, {title : 'liver', details : 'one human liver'}, {title : 'drugs', details : 'so many drugs'} ];
+	//var arr = [{title : 'teddy', details : 'A Really Big Teddy Bear'}, {title : 'car', details : 'A Super Fast car'}, {title : 'coat', details : 'A leather coat'}, {title : 'flowers', details : '1,000 Roses'}, {title : 'shoes', details : 'Air Jordans, size 9'}, {title : 'marbles', details : 'a million marbles'}, {title : 'liver', details : 'one human liver'}, {title : 'drugs', details : 'so many drugs'} ];
 	
 		
 		$.ajax({
@@ -77,7 +55,7 @@ router.route('location/locker', function () {
   		}).done(function (data){
 			console.log(data);
 			showLockers(data);
-			lockerGenerator(arr, 8);
+			lockerGenerator(data);
 			$(document).ready(function() {
 		            $('.vlocker').click(function() {
 						$(this).animate('flip');
@@ -179,13 +157,13 @@ var views = require('views');
 
 
 
-module.exports = function (arr, lockernumbers) {
+module.exports = function (arr) {
   var i = 0;
 
-	while ( i < lockernumbers) {
+	while ( i < arr.length) {
 
-		var lockerTitle= arr[i].title;
-		var lockerDetails= arr[i].details;
+		var lockerTitle= arr[i].id;
+		var lockerDetails= arr[i].actions;
 
 		var squareHtml = '<div class="vlocker"><span class="card animated"><span class="lockerTitle">'+ lockerTitle +'</span><div class="vpopout"><span class="lockerDetails">'+ lockerDetails+'</span><button class="buy-button">Buy</button></div></div>';
 		console.log(squareHtml);
@@ -265,12 +243,7 @@ var SortedRouter = Backbone.Router.extend({
 });
  
 module.exports = SortedRouter;
-<<<<<<< HEAD
->>>>>>> frontend:sharelockers/sharelockers/static/js/app.js
-},{"backbone":"backbone","underscore":"underscore"}]},{},[5])
-=======
 },{"backbone":"backbone","underscore":"underscore"}]},{},[7])
->>>>>>> 4a3417439ce5954a4b3c089a7ea931b6c93b311b
 
 
 //# sourceMappingURL=app.js.map
--- src/js/lockerGenerator.js
+++ src/js/lockerGenerator.js
@@ -6,13 +6,13 @@ var views = require('views');
 
 
 
-module.exports = function (arr, lockernumbers) {
+module.exports = function (arr) {
   var i = 0;
 
-	while ( i < lockernumbers) {
+	while ( i < arr.length) {
 
-		var lockerTitle= arr[i].title;
-		var lockerDetails= arr[i].details;
+		var lockerTitle= arr[i].id;
+		var lockerDetails= arr[i].actions;
 
 		var squareHtml = '<div class="vlocker"><span class="card animated"><span class="lockerTitle">'+ lockerTitle +'</span><div class="vpopout"><span class="lockerDetails">'+ lockerDetails+'</span><button class="buy-button">Buy</button></div></div>';
 		console.log(squareHtml);

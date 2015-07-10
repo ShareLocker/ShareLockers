45424f941fe5e81a753e5350e763af65613e1ff5
John Waldrep <johnwaldrep@gmail.com>
2015-07-10 17:02:41 -0400

    Add nested item_set to locker serializer

--- sharelockers/api/serializers.py
+++ sharelockers/api/serializers.py
@@ -7,33 +7,6 @@ from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 
-class LockerSerializer(serializers.ModelSerializer):  # FIXME: add Hyperlinked
-    actions = SerializerMethodField()
-    item = SerializerMethodField()
-
-    def get_actions(self, obj):
-        locker = obj
-        if locker.item_set.all(): # Has item(s)
-            if locker.item_set.first().owner.user is None:  # FIXME: Change related name for item_set or make 1:1
-                return ['can_stock']  # FIXME: Is this needed
-            elif locker.item_set.first().owner.user == self.context['request'].user:
-                return ['can_open']
-            else:
-                return ['can_buy']
-        return ['can_stock', 'can_open']
-
-    def get_item(self, obj):
-        locker = obj
-        item = None
-        if locker.item_set.all().exists():
-            item = locker.item_set.first()
-        return ItemSerializer(instance=item)
-
-
-    class Meta:
-        model = Locker
-        fields = ('id', 'hub', 'row', 'column', 'owner', 'actions', 'item')
-
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -44,14 +17,6 @@ class ProfileSerializer(serializers.ModelSerializer):  # FIXME: add Hyperlinked
     class Meta:
         model = Profile
         fields = ('id', 'user', 'rating', 'description', 'alias')
-
-
-class HubSerializer(serializers.ModelSerializer):
-    locker_set = LockerSerializer(many=True, read_only=True)  # A nested list of 'locker' items.
-
-    class Meta:
-        model = Hub
-        fields = ('id', 'name', 'location', 'ip', 'locker_set')
 
 
 class OwnedItemsSerializer(serializers.ModelSerializer):
@@ -68,7 +33,38 @@ class OwnedItemsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
 
+class LockerSerializer(serializers.ModelSerializer):  # FIXME: add Hyperlinked
+    actions = SerializerMethodField()
+    item_set = OwnedItemsSerializer(many=True, read_only=True)
+
+    def get_actions(self, obj):
+        locker = obj
+        if locker.item_set.all(): # Has item(s)
+            if locker.item_set.first().owner.user is None:  # FIXME: Change related name for item_set or make 1:1
+                return ['can_stock']  # FIXME: Is this needed
+            elif locker.item_set.first().owner.user == self.context['request'].user:
+                return ['can_open']
+            else:
+                return ['can_buy']
+        return ['can_stock', 'can_open']
+
+
+    class Meta:
+        model = Locker
+        fields = ('id', 'hub', 'row', 'column', 'owner', 'actions', 'item_set')
+
+class HubSerializer(serializers.ModelSerializer):
+    locker_set = LockerSerializer(many=True, read_only=True)  # A nested list of 'locker' items.
+
+    class Meta:
+        model = Hub
+        fields = ('id', 'name', 'location', 'ip', 'locker_set')
+
+
+
 class ItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Item
+
+

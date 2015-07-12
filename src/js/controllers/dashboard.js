3bc6add74777974949ba6a4c0073b3a6496212a4
Falon Thomas <falont23@gmail.com>
2015-07-12 18:36:58 -0400

    dashboard added

--- src/js/controllers/dashboard.js
+++ src/js/controllers/dashboard.js
@@ -6,14 +6,54 @@ var views = require('views');
 var router = require('../router');
 var show = require('../show');
 
+
 router.route('dashboard', function () {
 		
 	show('dashboard');
 	$.ajax({
 			method: 'GET', 
-			url: '/api/dashboard',
+			url: '/api/profiles/',
   		}).done(function (data){
 			console.log(data);
+
+// RESPONSIVE DASHBOARD MENU
+			
+			(function () {
+    
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    menuLink.onclick = function (e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+    };
+
+}(this, this.document));
+
 		});
 	
 

8bd3c26fd7f6bff7f4a25cf52559c17b336f6282
Alan R <alan.rominger@gmail.com>
2015-07-13 22:38:20 -0400

    location and hub admin management pages

--- sharelockers/lockers/urls.py
+++ sharelockers/lockers/urls.py
@@ -7,4 +7,8 @@ from hubs.models import Hub
 
 urlpatterns = [
     url(r'^hub_list_bs.html$', locker_views.HubListView.as_view(), name="view_hubs"),
+    url(r'^hub_edit/(?P<pk>\d+)$', locker_views.HubEditView.as_view(), name="edit_hub"),
+
+    url(r'^loc_list_bs.html$', locker_views.LocListView.as_view(), name="view_locs"),
+    url(r'^loc_edit/(?P<pk>\d+)$', locker_views.LocEditView.as_view(), name="edit_loc"),
 ]
--- sharelockers/lockers/views.py
+++ sharelockers/lockers/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from hubs.models import Hub
+import django.views.generic as django_views
+from django.views.generic.edit import UpdateView, CreateView
+from hubs.models import Hub, Location
+from lockers.forms import LocationForm
 
 # Create your views here.
-class HubListView(ListView):
+class HubListView(django_views.ListView):
 	model=Hub
 	template_name="lockers/hub_list_bs.html"
 	context_object_name='hubs'
@@ -15,3 +17,27 @@ class HubListView(ListView):
 				hub.connected = False
 				hub.save()
 		return super(HubListView, self).dispatch(*args, **kwargs)
+
+
+class HubEditView(UpdateView):
+	model = Hub
+	fields = ['name', 'location']
+	template_name="lockers/hub_edit_bs.html"
+	success_url = '/hub_list_bs.html'
+
+
+class LocListView(CreateView):
+	form_class = LocationForm
+	success_url = "/loc_list_bs.html"
+	template_name="lockers/loc_list_bs.html"
+
+	def get_context_data(self, **kwargs):
+		kwargs['locations'] = Location.objects.all()
+		return super(LocListView, self).get_context_data(**kwargs)
+
+
+class LocEditView(UpdateView):
+	model = Location
+	fields = ['description', 'latitude', 'longitude', 'host']
+	template_name="lockers/loc_edit_bs.html"
+	success_url = '/loc_list_bs.html'

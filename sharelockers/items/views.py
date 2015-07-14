b984176b0b82b3895448a133d61d6cfb585c9448
Manish Patel <maddypatel@gmail.com>
2015-07-13 21:07:56 -0400

    Can upload image

--- sharelockers/items/views.py
+++ sharelockers/items/views.py
@@ -1,3 +1,20 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from items.forms import ItemForm
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('created'))
+    else:
+        form = ItemForm()
+
+    return render(request, 'item.html', {
+        'form': form,
+    })

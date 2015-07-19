20719d1c9b422535a8e963a4966533b99b2a4303
Brendan Collins <brendan.c.collins@gmail.com>
2015-07-19 16:36:21 -0400

    attempting to login from buy screen

--- src/js/buyItem.js
+++ src/js/buyItem.js
@@ -6,32 +6,70 @@ var views = require ('views');
 var getCookie = require ('../js/getCookie');
 
 module.exports = function (button) {
+	var csrftoken = getCookie('csrftoken'); 
 	$(document).on('click', button ,function () {
-			console.log(this);
-			var itemId = this.getAttribute('data-id');
-			var buyerId = $('.user-id').attr('data-id');
-			var csrftoken = getCookie('csrftoken'); 
-			console.log(csrftoken);
+		console.log($('.user-id').length);
+		if ($('.user-id').length === 1) {
+				$('.not-loggedin-container').css("width", "100%");
+				$('.stock-wrapper').show();
+			 $('.close').click(function(){
+				 $('.stock-wrapper').hide();
+				 $('.not-loggedin-container').css("width", "0%");
+				})
+		$('.lgn-button').click(function(){
+				var data = new FormData();
+				var username = $('.username-login').val();
+				var password =$('.user-password').val();
+				data.append('username', username);
+				data.append('password', username);
 				$.ajax({
 					beforeSend: function (request){
 					console.log(csrftoken)
 		            request.setRequestHeader('X-CSRFToken', csrftoken);
 		           },
 					method: 'POST', 
-					url: '/api/purchases/',
+					url: '/login/',
+					//dataType: 'json',
 					data: {
-					   "item": itemId,
-					   "buyer": buyerId
+					   "username": username,
+					   "password": password
 					}
+					
 		  		}).done(function (data){
 					console.log(data);
-					setTimeout('parent.location.reload()',500);
-					alert("The Item is Yours!")
+					alert('yay');
+					//setTimeout('parent.location.reload()',500);
 				}).fail(function(data){
+					alert('oh no');
 					console.log(data);
 				});
+		})
+		}
+		
+		// 	var itemId = this.getAttribute('data-id');
+		// 	var buyerId = $('.user-id').attr('data-id');
+		// 	console.log(csrftoken);
+		// 		$.ajax({
+		// 			beforeSend: function (request){
+		// 			console.log(csrftoken)
+		//             request.setRequestHeader('X-CSRFToken', csrftoken);
+		//            },
+		// 			method: 'POST', 
+		// 			url: '/api/purchases/',
+		// 			data: {
+		// 			   "item": itemId,
+		// 			   "buyer": buyerId
+		// 			}
+		//   		}).done(function (data){
+		// 			console.log(data);
+		// 			setTimeout('parent.location.reload()',500);
+		// 			alert("The Item is Yours!")
+		// 		}).fail(function(data){
+		// 			console.log(data);
+		// 		});
 				
-				document.location.href = '/#/dashboard'
-		});
+		// 		document.location.href = '/#/dashboard'
+		// });
 	
+});
 }

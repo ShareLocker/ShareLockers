dab3326457bff572eb75f57adbeb05fc0458657e
Brendan Collins <brendan.c.collins@gmail.com>
2015-07-19 19:49:42 -0400

    trying to get credits to display in buying

--- src/js/buyItem.js
+++ src/js/buyItem.js
@@ -7,14 +7,15 @@ var getCookie = require ('../js/getCookie');
 
 module.exports = function (button) {
 	var csrftoken = getCookie('csrftoken'); 
-	$(document).on('click', button ,function () {
-		console.log($('.user-id').length);
-		if ($('.user-id').length === 1) {
+	$(document).on('click', button, function (e) {
+		console.log($('.user-id').html().length);
+		if ($('.user-id').html().length === 1) {
 				$('.not-loggedin-container').css("width", "100%");
 				$('.stock-wrapper').show();
 			 $('.close').click(function(){
 				 $('.stock-wrapper').hide();
 				 $('.not-loggedin-container').css("width", "0%");
+				 $('.buy-confirmation-container').css("width", "0%");
 				})
 		$('.lgn-button').click(function(){
 				var data = new FormData();
@@ -29,23 +30,19 @@ module.exports = function (button) {
 		           },
 					method: 'POST', 
 					url: '/login/',
-					//dataType: 'json',
 					data: {
 					   "username": username,
 					   "password": password
 					}
 					
-		  		}).done(function (data){
-					console.log(data);
-					alert('yay');
-					//setTimeout('parent.location.reload()',500);
-				}).fail(function(data){
-					alert('oh no');
+		  		}).then(getCredits()).fail(function(data){
+					alert('oh no! try again');
 					console.log(data);
 				});
-		})
+			});
 		}
 		
+
 		// 	var itemId = this.getAttribute('data-id');
 		// 	var buyerId = $('.user-id').attr('data-id');
 		// 	console.log(csrftoken);
@@ -73,3 +70,20 @@ module.exports = function (button) {
 	
 });
 }
+
+		function getCredits() {
+						alert('yay');
+						$.ajax({
+							method: 'GET',
+							dataType: 'text',
+							URL: 'api/mycredits/',
+							}).done(function (data){
+							console.log(data);
+						// var credits = $('.user-id').attr('data-credits');
+						// 	$('.credits').html(credits);
+							$('.buy-confirmation-container').css("width", "100%");
+						}).fail(function(data){
+							alert('oh no! try again');
+							console.log(data);
+						})
+			}

a34fb15c60139d209c2da59f999ec9cc443f3cf1
Alan R <alan.rominger@gmail.com>
2015-07-22 17:28:33 -0400

    comment out animations to get boxes working

--- sharelockers/static/js/app.js
+++ sharelockers/static/js/app.js
@@ -1,39 +1,40 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var $ = require("jquery");
-
-// HEADER ANIMATION
-$(function () {
-	
-	$(window).scroll(function() {
-	  if ($(this).scrollTop() > 1){
-	    $('.my-page-header').addClass("sticky");
-	  } else {
-	    $('.my-page-header').removeClass("sticky");
-	  }
-	});
-	
-	// Scroll Indicator
-	var homeIconContainerTop = $('.home-icons').offset().top - ($(window).height());
-	
-	var homeIconContainerTop2 = $('.home-icons2').offset().top - ($(window).height());
-	
-	$(window).scroll(function() {
-		
-		if ($(window).scrollTop () > homeIconContainerTop) {
-			$('.home-icons').addClass('slideRight');
-		}
-		if ($(window).scrollTop () > homeIconContainerTop2) {
-			$('.home-icons2').addClass('slideRight');
-		}
-			
-	});
-
-});
-
-// $(document).ready(function(){
-//   $(".scroll").animate( ".scroll" , 1000 , swing, complete);
+// var $ = require("jquery");
+//
+// // HEADER ANIMATION
+// $(function () {
+//
+// 	$(window).scroll(function() {
+// 	  if ($(this).scrollTop() > 1){
+// 	    $('.my-page-header').addClass("sticky");
+// 	  } else {
+// 	    $('.my-page-header').removeClass("sticky");
+// 	  }
+// 	});
+//
+// 	// Scroll Indicator
+// 	var homeIconContainerTop = $('.home-icons').offset().top - ($(window).height());
+//
+// 	var homeIconContainerTop2 = $('.home-icons2').offset().top - ($(window).height());
+//
+// 	$(window).scroll(function() {
+//
+// 		if ($(window).scrollTop () > homeIconContainerTop) {
+// 			$('.home-icons').addClass('slideRight');
+// 		}
+// 		if ($(window).scrollTop () > homeIconContainerTop2) {
+// 			$('.home-icons2').addClass('slideRight');
+// 		}
+//
+// 	});
+//
 // });
-},{"jquery":"jquery"}],2:[function(require,module,exports){
+//
+// // $(document).ready(function(){
+// //   $(".scroll").animate( ".scroll" , 1000 , swing, complete);
+// // });
+
+},{}],2:[function(require,module,exports){
 'use strict';
 
 var $ = require ('jquery');
--- src/js/animations.js
+++ src/js/animations.js
@@ -1,34 +1,34 @@
-var $ = require("jquery");
-
-// HEADER ANIMATION
-$(function () {
-	
-	$(window).scroll(function() {
-	  if ($(this).scrollTop() > 1){
-	    $('.my-page-header').addClass("sticky");
-	  } else {
-	    $('.my-page-header').removeClass("sticky");
-	  }
-	});
-	
-	// Scroll Indicator
-	var homeIconContainerTop = $('.home-icons').offset().top - ($(window).height());
-	
-	var homeIconContainerTop2 = $('.home-icons2').offset().top - ($(window).height());
-	
-	$(window).scroll(function() {
-		
-		if ($(window).scrollTop () > homeIconContainerTop) {
-			$('.home-icons').addClass('slideRight');
-		}
-		if ($(window).scrollTop () > homeIconContainerTop2) {
-			$('.home-icons2').addClass('slideRight');
-		}
-			
-	});
-
-});
-
-// $(document).ready(function(){
-//   $(".scroll").animate( ".scroll" , 1000 , swing, complete);
+// var $ = require("jquery");
+//
+// // HEADER ANIMATION
+// $(function () {
+//
+// 	$(window).scroll(function() {
+// 	  if ($(this).scrollTop() > 1){
+// 	    $('.my-page-header').addClass("sticky");
+// 	  } else {
+// 	    $('.my-page-header').removeClass("sticky");
+// 	  }
+// 	});
+//
+// 	// Scroll Indicator
+// 	var homeIconContainerTop = $('.home-icons').offset().top - ($(window).height());
+//
+// 	var homeIconContainerTop2 = $('.home-icons2').offset().top - ($(window).height());
+//
+// 	$(window).scroll(function() {
+//
+// 		if ($(window).scrollTop () > homeIconContainerTop) {
+// 			$('.home-icons').addClass('slideRight');
+// 		}
+// 		if ($(window).scrollTop () > homeIconContainerTop2) {
+// 			$('.home-icons2').addClass('slideRight');
+// 		}
+//
+// 	});
+//
 // });
+//
+// // $(document).ready(function(){
+// //   $(".scroll").animate( ".scroll" , 1000 , swing, complete);
+// // });

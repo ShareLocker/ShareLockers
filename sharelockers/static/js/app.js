d62b3095b9d3bc26d6e6c27d371dd07c570fec37
Falon Thomas <falont23@gmail.com>
2015-07-22 15:58:48 -0400

    home changes

--- sharelockers/static/js/app.js
+++ sharelockers/static/js/app.js
@@ -13,12 +13,17 @@ $(function () {
 	});
 	
 	// Scroll Indicator
-	//var homeIconContainerTop = $('.home-icons').offset().top - ($(window).height());
+	var homeIconContainerTop = $('.home-icons').offset().top - ($(window).height());
+	
+	var homeIconContainerTop2 = $('.home-icons2').offset().top - ($(window).height());
 	
 	$(window).scroll(function() {
 		
 		if ($(window).scrollTop () > homeIconContainerTop) {
 			$('.home-icons').addClass('slideRight');
+		}
+		if ($(window).scrollTop () > homeIconContainerTop2) {
+			$('.home-icons2').addClass('slideRight');
 		}
 			
 	});

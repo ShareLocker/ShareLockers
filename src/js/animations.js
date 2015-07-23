cf4bfadbb37768775f2b16e9d1fa7163ffc81929
Brendan Collins <brendan.c.collins@gmail.com>
2015-07-22 20:42:59 -0400

    merging in commenting out animatinos

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,33 +1,33 @@
-var $ = require("jquery");
+// var $ = require("jquery");
 
-// HEADER ANIMATION
-$(function () {
+// // HEADER ANIMATION
+// $(function () {
 	
-	$(window).scroll(function() {
-	  if ($(this).scrollTop() > 1){
-	    $('.my-page-header').addClass("sticky");
-	  } else {
-	    $('.my-page-header').removeClass("sticky");
-	  }
-	});
+// 	$(window).scroll(function() {
+// 	  if ($(this).scrollTop() > 1){
+// 	    $('.my-page-header').addClass("sticky");
+// 	  } else {
+// 	    $('.my-page-header').removeClass("sticky");
+// 	  }
+// 	});
 	
-	// Scroll Indicator
-	var homeIconContainerTop = $('.home-icons').offset().top - ($(window).height());
+// 	// Scroll Indicator
+// 	var homeIconContainerTop = $('.home-icons').offset().top - ($(window).height());
 	
-	var homeIconContainerTop2 = $('.home-icons2').offset().top - ($(window).height());
+// 	var homeIconContainerTop2 = $('.home-icons2').offset().top - ($(window).height());
 	
-	$(window).scroll(function() {
+// 	$(window).scroll(function() {
 		
-		if ($(window).scrollTop () > homeIconContainerTop) {
-			$('.home-icons').addClass('slideRight');
-		}
-		if ($(window).scrollTop () > homeIconContainerTop2) {
-			$('.home-icons2').addClass('slideRight');
-		}
+// 		if ($(window).scrollTop () > homeIconContainerTop) {
+// 			$('.home-icons').addClass('slideRight');
+// 		}
+// 		if ($(window).scrollTop () > homeIconContainerTop2) {
+// 			$('.home-icons2').addClass('slideRight');
+// 		}
 			
-	});
+// 	});
 
-});
+// });
 
 // $(document).ready(function(){
 //   $(".scroll").animate( ".scroll" , 1000 , swing, complete);
